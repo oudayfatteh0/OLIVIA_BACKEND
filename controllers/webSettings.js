1cb70a7e70831401ed6c16b10559757fdc7feeaf
oudayfatteh0 <oudayfatteh51@gmail.com>
2024-10-21 23:46:04 +0100

    fix

--- controllers/webSettings.js
+++ controllers/webSettings.js
@@ -25,7 +25,7 @@ exports.changeWebSettings = async (req, res) => {
 		}
 	} catch (error) {
 		console.error(error);
-		res.status(500).json({ message: 'Server error' });
+		res.status(500).json({ message: 'Server error', error: error });
 	}
 };
 
--- functions/app.js
+++ functions/app.js
@@ -11,12 +11,28 @@ const router = express.Router();
 
 app.use(express.json());
 
-mongoose
-	.connect(
-		`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.npoov.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
-	)
-	.then(() => console.log('MongoDB connected'))
-	.catch((err) => console.error('MongoDB connection error:', err));
+let isConnected;
+
+const connectToMongoDB = async () => {
+	if (isConnected) {
+		return;
+	}
+
+	try {
+		await mongoose.connect(
+			`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.npoov.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
+		);
+		isConnected = true;
+		console.log('MongoDB connected');
+	} catch (err) {
+		console.error('MongoDB connection error:', err);
+	}
+};
+
+app.use(async (req, res, next) => {
+	await connectToMongoDB();
+	next();
+});
 
 router.post('/login', loginController.login);
 
